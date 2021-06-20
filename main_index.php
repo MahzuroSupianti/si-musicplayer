b6e0f7c64fd219c335509d966f0fc35352b1f185
Naryama Harahap <naryamaharahap20@gmail.com>
2021-06-20 14:28:00 +0700

    Komentar perubahan

---
+++ main_index.php
@@ -0,0 +1,1 @@
+<h2> Selamat Datang di WEB MUSIK</h2>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'naryama/uts-web',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'naryama/uts-web' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
